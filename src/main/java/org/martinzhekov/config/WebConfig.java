22d2290646e10278157da9e1ce842fca993cf220
Martin Zhekov <martyj@mail.bg>
2020-11-14 22:39:25 +0200

    Added demo jsp view

--- src/main/java/org/martinzhekov/config/WebConfig.java
+++ src/main/java/org/martinzhekov/config/WebConfig.java
@@ -1,8 +1,12 @@
 package org.martinzhekov.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 /**
  * Created by martinzhekov on 14.11.20
@@ -12,5 +16,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @ComponentScan(basePackages = "org.martinzhekov")
 public class WebConfig {
+    //== constants ==
+    public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+    public static final String RESOLVER_SUFFIX = ".jsp";
 
+    // bean methods ==
+    @Bean
+    public ViewResolver viewResolver() {
+        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(RESOLVER_PREFIX);
+        viewResolver.setSuffix(RESOLVER_SUFFIX);
+        return viewResolver;
+    }
 }
